11ae9cc99f6eb509767943cb17f553c9027e9f09
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-01-17 23:12:17 -0600

    Rutas anidadas

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -4,13 +4,14 @@ import { Component, signal } from '@angular/core';
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
+import { HeaderComponent } from "@shared/components/header/header.component";
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [CommonModule, CounterComponent, WaveAudioComponent, HighlightDirective],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+    selector: 'app-about',
+    standalone: true,
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.css',
+    imports: [CommonModule, CounterComponent, WaveAudioComponent, HighlightDirective, HeaderComponent]
 })
 export class AboutComponent {
   duration = signal(1000);
