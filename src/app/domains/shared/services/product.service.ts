b8d0f6577f30341fbd130ecdde846670507c6580
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-01-20 10:03:43 -0600

    Obteniendo detalle de productos

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -8,8 +8,14 @@ import { Product } from '../models/product.model';
 export class ProductService {
 
   private http = inject(HttpClient);
+
   constructor() { }
+
   getProducts(){
     return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+  }
+
+  getOne(id: string){
+    return this.http.get<Product>(`https://api.escuelajs.co/api/v1/products/${id}`)
   }
 }
